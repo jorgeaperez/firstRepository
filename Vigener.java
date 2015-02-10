523f3f40ee4b90dfb4a72e5944dd818bc1b51d48
jorgeaperez <jpere682@fiu.edu>
2015-02-10 01:22:32 -0500

    Vigenere cypher test!

---
+++ Vigener.java
@@ -0,0 +1,27 @@
+
+public class Vigener {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		
+		
+		String word = "crypto";
+		String m = "whatanicedaytoday";
+		String result ="";
+		System.out.println((int)word.charAt(0));
+		System.out.println((m.toString().charAt(0)+word.toString().charAt(0))%26);
+		System.out.println(word.charAt(1)%26);
+		System.out.println((int)(m.charAt(1))-96);
+		System.out.println((int)(word.charAt(1))-96);
+		for(int i =0;i<m.length();i++){
+			for(int j=0;j<word.length();j++){
+			
+			}
+		}
+
+	}
+
+}
